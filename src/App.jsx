4b7ee61016a92b3effe9385f759a52bfcc567b57
MartinsCaique <caiquety1@gmail.com>
2026-08-20 19:43:03 -0300

    Add Hero component to enhance the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import { Header } from './components/Header'
+import { Hero } from './components/Hero'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <div className="h-0 w-160 absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] rotate-[-30deg] -z-10"></div>
 
         <Header />
+        <Hero />
         
       </main>
     </>
